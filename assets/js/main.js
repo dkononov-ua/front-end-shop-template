eca69aaf5488474ae1d4cdc56ba73e5f6195f708
Denys <dkononov.ua@gmail.com>
2022-12-01 11:59:12 +0200

    доробив лічильник

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,43 @@ const cartCounterLabel = document.querySelector("#cart-counter");
 let cartCounter = 0;
 let cartPrice = 0;
 
+const incrementCounter = (label, cn) => {
+  const counter = cn + 1;
+
+  label.innerHTML = `${counter}`;
+
+  if (counter === 1) cartCounterLabel.style.display = "block";
+
+  return counter;
+};
+
+const disableControls = (t, fn) => {
+  t.disabled = true;
+  contentContainer.removeEventListener("click", fn);
+};
+const enableControls = (t, fn) => {
+  t.disabled = false;
+  contentContainer.addEventListener("click", fn);
+};
+
 const btnClickHandler = (e) => {
   const target = e.target;
+  const interval = 2000;
+  let restoreHTML = null;
 
   if (typeof target !== "object") return;
 
   if (target && target.classList.contains("item-actions__cart")) {
-    cartCounter++;
-    console.log(cartCounter);
+    cartCounter = incrementCounter(cartCounterLabel, cartCounter);
 
-    cartCounterLabel.innerHTML = cartCounter;
+    disableControls(target, btnClickHandler);
+    restoreHTML = target.innerHTML;
+    target.innerHTML = "у кошику";
 
-    if (cartCounter === 1) {
-      cartCounterLabel.style.display = "block";
-    }
+    setTimeout(() => {
+      enableControls(target, btnClickHandler);
+      target.innerHTML = restoreHTML;
+    }, interval);
   }
 };
 
